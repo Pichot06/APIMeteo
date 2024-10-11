1c39aac288152c943e8bf2b1b6da4de6667991dc
Pichot06 <alexandre.pichotx@gmail.com>
2024-10-11 10:12:14 +0200

    meteo_nice

---
+++ js.js
@@ -0,0 +1,14 @@
+async function lecture() {
+    let requestOptions = { method: 'GET' };
+    let reponse = await
+        fetch(`Meteo IUT GET : https://api.openweathermap.org/data/2.5/weather?lat=43.686784&lon=7.228607&appid=e233ba9cbcc9c0cfe4a3fe4c6a062c97&units=metric&lang=fr`, requestOptions);
+    let valeur_JSON = await reponse.json();
+    return valeur_JSON;
+}
+async function getNiceWeather() {
+    const weatherData = await lecture();
+    document.getElementById('nice-weather').innerText = `Température à Nice: ${weatherData.main.temp}°C, Condition: ${weatherData.weather[0].description}`;
+}
+
+// Initialize weather data on page load for Nice
+getNiceWeather();
